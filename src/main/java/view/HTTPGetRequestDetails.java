9c65514c01edc47e22bcda9dd0dc4a7ac239d7cf
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-26 21:03:05 +0100

    Add summary document

--- src/main/java/view/HTTPGetRequestDetails.java
+++ src/main/java/view/HTTPGetRequestDetails.java
@@ -2,11 +2,10 @@ package view;
 
 import control.UrlProcessor;
 import model.ResponseDetails;
+import model.ValidResponse;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Main view class for using the program
@@ -21,6 +20,7 @@ public class HTTPGetRequestDetails {
         // Declare variables
         List<String> urls;
         List<ResponseDetails> responses;
+        Map<Integer, List<ValidResponse>> responsesByStatus;
         Scanner kb;
         String input;
 
@@ -34,6 +34,9 @@ public class HTTPGetRequestDetails {
         // Initialise arrays
         urls = new ArrayList<>();
         responses = new ArrayList<>();
+        responsesByStatus = new HashMap<>();
+
+        System.out.println("\n\n*** RESPONSES ***\n");
 
         // Read file and exit with error if file is not found.
         try {
@@ -53,9 +56,34 @@ public class HTTPGetRequestDetails {
             }
         }
 
-        // Print the response details of each URL
+        // Print the response details of each URL and map to relevant status code if it is a valid response
         for (ResponseDetails r : responses) {
+            // Print to console
             System.out.println(r.toJsonString());
+
+            // Add to map
+            if(r instanceof ValidResponse) {
+                // Get status code
+                int statusCode = ((ValidResponse) r).getStatusCode();
+
+                // Initialise new array in the map for statusCode if it doesn't already exist
+                if(!responsesByStatus.containsKey(statusCode)) {
+                    responsesByStatus.put(statusCode, new ArrayList<>());
+                }
+
+                // Add the response to the array mapped to the statusCode
+                responsesByStatus.get(statusCode).add((ValidResponse) r);
+            }
+        }
+
+        System.out.println("\n\n*** SUMMARY OF RESULTS ***\n");
+
+        // Print summary JSON for each status code
+        for(int statusCode : responsesByStatus.keySet()) {
+            System.out.println("{\n"
+                    + "\t\"Status_code\": \"" + statusCode + "\",\n"
+                    + "\t\"Number_of_responses\": " + responsesByStatus.get(statusCode).size() + "\n"
+                    + "}");
         }
     }
 }

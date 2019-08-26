e7b1d1f114da715763757def784b55e184645bd5
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-26 13:29:37 +0100

    Begin implementation of get request method

--- src/main/java/control/UrlProcessor.java
+++ src/main/java/control/UrlProcessor.java
@@ -1,8 +1,15 @@
 package control;
 
+import model.InvalidResponse;
+import model.ResponseDetails;
+
 import java.io.*;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -35,5 +42,35 @@ public class UrlProcessor {
 
         // Return the list of URLs
         return urls;
+    }
+
+    public static ResponseDetails makeGetRequest(String urlStr) throws IOException {
+        URL url;
+        HttpURLConnection con;
+        List<String> content;
+        BufferedReader br;
+        Calendar date;
+        String line;
+        int status;
+
+        try {
+            url = new URL(urlStr);
+        } catch (MalformedURLException e) {
+            return new InvalidResponse(urlStr, "Invalid URL");
+        }
+
+        con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        status = con.getResponseCode();
+
+        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        content = new ArrayList<>();
+
+        while((line = br.readLine()) != null) {
+            content.add(line);
+        }
+
+        return null;
     }
 }
--- src/test/java/UrlProcessorTest.java
+++ src/test/java/UrlProcessorTest.java
@@ -53,5 +53,4 @@ public class UrlProcessorTest {
 
         assertEquals(urls, urlsFromFile);
     }
-
 }
